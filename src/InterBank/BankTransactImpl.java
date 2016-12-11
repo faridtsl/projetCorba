16d464c2a9badd852b75388a1cecb1414a2fcf80
PETIT Mathieu <shinato.petit@gmail.com>
2016-12-11 21:47:51 +0100

    Possibilité d'obtenir les logs d'interbank

--- src/InterBank/BankTransactImpl.java
+++ src/InterBank/BankTransactImpl.java
@@ -6,6 +6,7 @@ import org.omg.CORBA.ORB;
 public class BankTransactImpl extends BankTransactPOA {
 
 	HashMap<Integer,List<Transact>> transacts;
+	List<Transact> log = new LinkedList<Transact>();
 	ORB orb;
 	
 	public BankTransactImpl(ORB orb){
@@ -17,6 +18,7 @@ public class BankTransactImpl extends BankTransactPOA {
 	public void saveTransact(int bank_from, int from, int bank_id, int to, int amount) {
 		// TODO Auto-generated method stub
 		Transact t = new Transact(from, bank_from, to, bank_id, amount, 0);
+		log.add(t);
 		List<Transact> lto = transacts.get(bank_id);
 
 		List<Transact> lfrom = transacts.get(bank_from);
@@ -34,23 +36,28 @@ public class BankTransactImpl extends BankTransactPOA {
 
 	@Override
 	public Transact[] passTransacts(int bank_id) {
-		List<Transact> l = transacts.get(bank_id);
-		if(l == null) 
-			return new Transact[0];
+		
 		List<Transact> res = new LinkedList<Transact>();
-		for( Transact t : l ){
-			if(t.getDone() == 0 && bank_id == t.getBank_to()){
-				res.add(t);
-				t.setDone(1);
-			}
-			if(t.getDone() == 1 && bank_id == t.getBank_from()){
-				res.add(t);
-				t.setDone(2);
+		if (bank_id == 0) {
+			res = log;
+		} else {
+			
+			List<Transact> l = transacts.get(bank_id);
+			if(l == null) 
+				return new Transact[0];
+			
+			for( Transact t : l ){
+				if(t.getDone() == 0 && bank_id == t.getBank_to()){
+					res.add(t);
+					t.setDone(1);
+				}
+				if(t.getDone() == 1 && bank_id == t.getBank_from()){
+					res.add(t);
+					t.setDone(2);
+				}
 			}
 		}
 		Transact[] arr = new Transact[res.size()];
 		return res.toArray(arr);
 	}
-
-
 }
